f790b87ece473c416bca6af999d0194c885e6369
Lev Potapov <30991434+malaev@users.noreply.github.com>
2020-05-28 23:24:10 +0500

    Update tracer.py
    add org and country info

--- tracer.py
+++ tracer.py
@@ -1,5 +1,29 @@
 import subprocess
 import re
+import urllib.request
+import json
+
+
+def getInfoByIp(ip):
+    url = "https://ipinfo.io/" + ip + "/json"
+    getInfo = ''
+    try:
+        getInfo = urllib.request.urlopen(url)
+    except:
+        getInfo = ''
+    infoList = json.load(getInfo)
+
+    country = ''
+    org = ''
+    try:
+        country = infoList['country']
+    except:
+        country = ''
+    try:
+        org = infoList['org']
+    except:
+        org = ''
+    return  [country, org]
 
 print('Введите доменное имя или ip адрес')
 domen = input()
@@ -10,5 +34,6 @@ lines = re.findall(r'[A-Za-z0-9.-]+ \[\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\]',
 lines = [elem.strip(r'\[|\]') for elem in lines]
 index = 1
 for elem in lines[1:]:
-    print(str(index) + ' ' + elem.split(' [')[0] + ' ' + elem.split(' [')[1])
+    ipInfo = getInfoByIp(elem.split(' [')[1])
+    print(str(index) + ' ' + elem.split(' [')[0] + ' ' + elem.split(' [')[1] + ' ' + ipInfo[0] + ' ' + ipInfo[1])
     index += 1
